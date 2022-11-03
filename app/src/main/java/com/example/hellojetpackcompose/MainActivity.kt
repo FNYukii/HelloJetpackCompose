9e934c857f2d4eb7e7e5687735451e23ec4f38e9
Yu357 <forestgreen14689@gmail.com>
2022-11-03 22:29:25 +0900

    clean: 不要なインポートを削除

--- app/src/main/java/com/example/hellojetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/hellojetpackcompose/MainActivity.kt
@@ -3,16 +3,7 @@ package com.example.hellojetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Notifications
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
 import com.example.hellojetpackcompose.components.NavigationView
 import com.example.hellojetpackcompose.ui.theme.HelloJetpackComposeTheme
 
