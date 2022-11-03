20d9ea033cc89d00827435582de34b491cc6f2f2
Yu357 <forestgreen14689@gmail.com>
2022-11-03 22:39:38 +0900

    feat: Button置いてみた

--- app/src/main/java/com/example/hellojetpackcompose/screens/FirstScreen.kt
+++ app/src/main/java/com/example/hellojetpackcompose/screens/FirstScreen.kt
@@ -1,8 +1,10 @@
 package com.example.hellojetpackcompose
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -10,13 +12,24 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun FirstScreen() {
-    
-    val fruits: List<String> = listOf("Apple", "Orange", "Banana", "Melon", "Berry")
 
-    LazyColumn(modifier = Modifier.padding(8.dp)) {
-        items(items = fruits) { name ->
-            
-            Text(text = name)
+    Column(modifier = Modifier.padding(8.dp)) {
+
+        LazyColumn {
+
+            val fruits: List<String> = listOf("Apple", "Orange", "Banana", "Melon", "Berry")
+
+            items(items = fruits) { name ->
+
+                Text(text = name)
+            }
         }
+
+        Button(
+            onClick = {}
+        ) {
+            Text("Click")
+        }
+
     }
 }
