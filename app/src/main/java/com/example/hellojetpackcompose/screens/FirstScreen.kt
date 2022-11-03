d8a438592f23426ded82638c572d5a970b199aeb
Yu357 <forestgreen14689@gmail.com>
2022-11-03 22:34:33 +0900

    feat: List置いてみた

--- app/src/main/java/com/example/hellojetpackcompose/screens/FirstScreen.kt
+++ app/src/main/java/com/example/hellojetpackcompose/screens/FirstScreen.kt
@@ -1,10 +1,22 @@
 package com.example.hellojetpackcompose
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun FirstScreen() {
+    
+    val fruits: List<String> = listOf("Apple", "Orange", "Banana", "Melon", "Berry")
 
-    Text(text = "Home")
+    LazyColumn(modifier = Modifier.padding(8.dp)) {
+        items(items = fruits) { name ->
+            
+            Text(text = name)
+        }
+    }
 }
