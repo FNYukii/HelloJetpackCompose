a2a62fa29afe489ef1a3277f2afcfb8837f7133a
Yu357 <forestgreen14689@gmail.com>
2022-11-05 22:19:20 +0900

    feat: Firestoreのデータを取得して表示できた

--- app/src/main/java/com/example/hellojetpackcompose/screens/FirstScreen.kt
+++ app/src/main/java/com/example/hellojetpackcompose/screens/FirstScreen.kt
@@ -1,35 +1,51 @@
 package com.example.hellojetpackcompose
 
+import android.annotation.SuppressLint
+import android.content.ContentValues
+import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
+@SuppressLint("MutableCollectionMutableState")
 @Composable
 fun FirstScreen() {
 
-    Column(modifier = Modifier.padding(8.dp)) {
+    var todos by remember { mutableStateOf(ArrayList<String>()) }
 
-        LazyColumn {
-
-            val fruits: List<String> = listOf("Apple", "Orange", "Banana", "Melon", "Berry")
-
-            items(items = fruits) { name ->
-
-                Text(text = name)
+    val db = Firebase.firestore
+    db.collection("todos")
+        .addSnapshotListener { value, e ->
+            if (e != null) {
+                Log.w(ContentValues.TAG, "Listen failed.", e)
+                return@addSnapshotListener
             }
+
+            val newTodos = ArrayList<String>()
+            for (doc in value!!) {
+                doc.getString("text")?.let {
+                    newTodos.add(it)
+                }
+            }
+
+            todos = newTodos
         }
 
-        Button(
-            onClick = {}
-        ) {
-            Text("Click")
-        }
+    LazyColumn {
 
+        items(items = todos) { name ->
+
+            Text(text = name)
+        }
     }
+
+
 }
