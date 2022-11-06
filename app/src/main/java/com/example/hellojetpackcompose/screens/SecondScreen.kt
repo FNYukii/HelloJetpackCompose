8d4d7da67133f0592cb210f84c8358cb44ba03f6
Yu357 <forestgreen14689@gmail.com>
2022-11-06 22:07:00 +0900

    feat: TextFieldを配置

--- app/src/main/java/com/example/hellojetpackcompose/screens/SecondScreen.kt
+++ app/src/main/java/com/example/hellojetpackcompose/screens/SecondScreen.kt
@@ -1,13 +1,22 @@
 package com.example.hellojetpackcompose
 
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun SecondScreen() {
 
-    Text(text = "Create", Modifier.padding(8.dp))
+    var text by remember { mutableStateOf("") }
+
+    OutlinedTextField(
+        value = text,
+        onValueChange = { text = it },
+        label = { Text("Todo") },
+        modifier = Modifier.padding(16.dp).fillMaxWidth()
+    )
 }
