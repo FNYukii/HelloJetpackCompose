a4b5b286b41ab10d98f92c643db381f27ce42bf8
Yu357 <forestgreen14689@gmail.com>
2022-11-06 16:15:04 +0900

    feat: ViewModelを使ってTodosを表示した

--- app/src/main/java/com/example/hellojetpackcompose/screens/FirstScreen.kt
+++ app/src/main/java/com/example/hellojetpackcompose/screens/FirstScreen.kt
@@ -1,45 +1,22 @@
 package com.example.hellojetpackcompose.screens
 
-import android.annotation.SuppressLint
-import android.content.ContentValues
-import android.util.Log
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
-import com.example.hellojetpackcompose.entities.Todo
-import com.example.hellojetpackcompose.utilities.FireTodos
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.hellojetpackcompose.viewModels.TodosViewModel
 
-@SuppressLint("MutableCollectionMutableState")
 @Composable
-fun FirstScreen() {
+fun FirstScreen(
+    todosViewModel: TodosViewModel = hiltViewModel()
+) {
 
-    var todos by remember { mutableStateOf(ArrayList<Todo>()) }
-
-    val db = Firebase.firestore
-    db.collection("todos")
-        .addSnapshotListener { value, e ->
-            if (e != null) {
-                Log.w(ContentValues.TAG, "Listen failed.", e)
-                return@addSnapshotListener
-            }
-
-            val newTodos = ArrayList<Todo>()
-            for (doc in value!!) {
-
-                val todo = FireTodos.toTodo(doc)
-                newTodos.add(todo)
-            }
-
-            todos = newTodos
-        }
+    val todos by todosViewModel.todos.collectAsState()
+    todosViewModel.load()
 
     LazyColumn {
-
         items(items = todos) { todo ->
-
             Text(text = todo.text)
         }
     }
