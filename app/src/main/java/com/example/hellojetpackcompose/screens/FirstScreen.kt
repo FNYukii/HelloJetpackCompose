456f61331272842af9ac53df4ae900ddae9b432a
Yu357 <forestgreen14689@gmail.com>
2022-11-06 16:54:45 +0900

    feat: RowにDividerを追加

--- app/src/main/java/com/example/hellojetpackcompose/screens/FirstScreen.kt
+++ app/src/main/java/com/example/hellojetpackcompose/screens/FirstScreen.kt
@@ -1,11 +1,16 @@
 package com.example.hellojetpackcompose.screens
 
+import android.graphics.Color
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color.Companion.Black
+import androidx.compose.ui.text.font.FontWeight.Companion.Black
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.hellojetpackcompose.viewModels.TodosViewModel
@@ -20,7 +25,11 @@ fun FirstScreen(
 
     LazyColumn {
         items(items = todos) { todo ->
-            Text(text = todo.text, Modifier.padding(8.dp))
+
+            Column {
+                Text(text = todo.text, Modifier.padding(16.dp))
+                Divider()
+            }
         }
     }
 }
